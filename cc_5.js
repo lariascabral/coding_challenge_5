e331eb7a1cc387d0c5af09107f91338bdb7a9d80
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-02-09 18:24:00 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -4,7 +4,23 @@ let customer = {
     name: "John Doe", 
     age: 35, 
     email: "john.doe@email.com" 
-} ;                                                         // Creation of customer's profile
+} ;                                      // Creation of customer's profile
 
 for (const property in customer) {
     console.log(`${property}: ${customer[property]}`)} ;    // Each property in the object will be log individually
+
+
+// Task 2 - Order Details
+
+let order ={ 
+    orderId: 12345, 
+    totalAmount: 150, 
+    status: "Processing",                 // Creation of order as object
+    displayOrder: function() {
+        console.log(`Order ID: ${this.orderId}`) ;
+        console.log(`Order Total Amount: $${this.totalAmount}`) ;
+        console.log(`Order Status: ${this.status}`) ;
+    }
+} ;                                      // Addition of displayOrder method using this
+
+order.displayOrder() ;                   // Calling the method displayOrder
